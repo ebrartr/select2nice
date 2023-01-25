9fb3178058994ff615bbffd90ed95f76d37e8dce
Ebrar Haşlak <ebrar.haslak@sanayi.gov.tr>
2023-01-25 12:37:43 +0300

    infobox and tools some edits

--- WebApplication1/wwwroot/lib/select2nice/select2nice.js
+++ WebApplication1/wwwroot/lib/select2nice/select2nice.js
@@ -35,6 +35,7 @@
         siblingObj.addClass('siblingClassCustom');
 
         niceInfoBox = getSiblingObjCopy(siblingObj);
+        niceInfoBox.addClass('infoBox');
         niceInfoBox.settings = settings;
         niceInfoBox.insertAfter(siblingObj);
         niceInfoBox.refresh = function () {
@@ -78,27 +79,47 @@
         });
 
         niceInfoBox.tools = getSiblingObjCopy(siblingObj);
+        niceInfoBox.tools.addClass('infoBoxTools');
+        niceInfoBox.tools.mainDiv = $('<div class="row"></div>');
 
-        niceInfoBox.tools.mainDiv = $('<div class="toolMainDiv"></div>');
-        niceInfoBox.tools.deleteDiv = $('<div class="col-12 text-center deleteButton"></div>');
+
+        // DELETE button start
+
+        niceInfoBox.tools.deleteDiv = $('<div class="col-6 text-center deleteButton"></div>');
         niceInfoBox.tools.deleteIcon = $(`<svg width="16" height="16" fill="currentColor" class="deleteButton" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
   <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
 </svg>`);
-
         niceInfoBox.tools.deleteIcon.click(function () {
 
             baseObj.val('');
             baseObj.trigger('change');
             niceInfoBox.refresh();
         });
-
         niceInfoBox.tools.deleteDiv.append(niceInfoBox.tools.deleteIcon);
         niceInfoBox.tools.mainDiv.append(niceInfoBox.tools.deleteDiv);
-        niceInfoBox.tools.append(niceInfoBox.tools.mainDiv);
-
         niceInfoBox.tools.deleteDiv.hide();
 
+        //DELETE button end
+
+        // SELECT ALL button start
+
+        niceInfoBox.tools.selectAllDiv = $('<div class="col-6 text-center selectAllButton"></div>');
+        niceInfoBox.tools.selectAllIcon = $(`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16">
+  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"></path>
+</svg>`);
+        niceInfoBox.tools.selectAllIcon.click(function () {
+
+            alert('select all');
+
+        });
+
+        niceInfoBox.tools.selectAllDiv.append(niceInfoBox.tools.selectAllIcon);
+        niceInfoBox.tools.mainDiv.append(niceInfoBox.tools.selectAllDiv);
+
+        // SELECT ALL button end
+
+        niceInfoBox.tools.append(niceInfoBox.tools.mainDiv);
         niceInfoBox.tools.insertAfter(niceInfoBox);
 
         niceInfoBox.refresh();// eğer mevcutta seçili bir select init edilecekse
